d3678812325a42bd0fe5504536a7ec55eb337e67
부엉이 <o.o.wl.soft@gmail.com>
2019-09-23 21:25:30 +0900

    feat: Apply Rule of SwiftLint

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 이동영 on 23/09/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let a: Int?
+        // Do any additional setup after loading the view.
+    }
+
+}
