2aa00b175d617237a789d10e130dce1c704eb243
부엉이 <o.o.wl.soft@gmail.com>
2019-09-25 23:20:07 +0900

    feat: Define product Cell

---
+++ VendingMachineApp/VendingMachineApp/Source/View/Component/ProductCell.swift
@@ -0,0 +1,34 @@
+//
+//  ProductCell.swift
+//  VendingMachineApp
+//
+//  Created by 이동영 on 25/09/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    // MARK: Properties
+
+    // MARK: IBOutlet
+    @IBOutlet weak var addQuantityButton: UIButton!
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productQuantityLabel: UILabel!
+
+    // MARK: Method
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configure(_ product: Product) {
+        productImageView.image = UIImage(named: "\(type(of: product))")
+    }
+
+    // MARK: IBAction
+    @IBAction func addQuantityButtonDidTap(_ sender: UIButton) {
+
+    }
+}
