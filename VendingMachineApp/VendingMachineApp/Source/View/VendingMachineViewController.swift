3422afe7237f0d8069639e0c4bc81f2310a6ef47
부엉이 <o.o.wl.soft@gmail.com>
2019-09-26 12:38:17 +0900

    feat: Adopt UICollectionDataSource to VendingMachineViewController #4

--- VendingMachineApp/VendingMachineApp/Source/View/VendingMachineViewController.swift
+++ VendingMachineApp/VendingMachineApp/Source/View/VendingMachineViewController.swift
@@ -14,45 +14,71 @@ protocol VendingMachineView {
 }
 
 class VendingMachineViewController: UIViewController {
-    
+
     // MARK: Properties
-    var vendingMachine = VendingMachine(balance: .init(value: 0),
-                                        inventory: Inventory.init(products: ProductFactory.createAll(quantity: 1)),
-                                        history: History())
+    var presenter: VendingMachinePresenterType!
+        = VendingMachinePresenter(balance: Money(value: 0),
+                         inventory: Inventory(products: ProductFactory.createAll(quantity: 1)),
+                         history: .init())
+
     // MARK: IBOutlet
     @IBOutlet weak var productsCollectionView: UICollectionView!
-    @IBOutlet weak var buttonToAdd1000Won: UIButton!
-    @IBOutlet weak var buttonToAdd5000Won: UIButton!
+    @IBOutlet weak var add1000WonButton: UIButton!
+    @IBOutlet weak var add5000WonButton: UIButton!
     @IBOutlet weak var balanceLabel: UILabel!
-    
+
     // MARK: Methods
     // MARK: IBAction
     // MARK: ViewController Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
+    }
+
+    func setupCollectionView() {
         productsCollectionView.dataSource = self
+        productsCollectionView.delegate = self
+
+        let productCellNib = UINib(nibName: ProductCell.nibName,
+                                   bundle: .main)
+        productsCollectionView.register(productCellNib,
+                                        forCellWithReuseIdentifier: ProductCell.reuseId)
+        productsCollectionView.isScrollEnabled = false
     }
-    
-}
-extension VendingMachineViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        var menuCount = 0
-        vendingMachine.handleProductStatistic({ _ in menuCount+=1 })
-        return menuCount
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
-    
+
 }
 // MARK: - + VendingMachineView
 extension VendingMachineViewController: VendingMachineView {
     func displayProducts() {
-        
+
     }
     func displayBalance() {
-        
+
+    }
+}
+// MARK: - + UICollectionViewDataSource
+extension VendingMachineViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return presenter.numOfRow
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductCell.reuseId,
+                                                      for: indexPath)
+        guard
+            let productCell = cell as? ProductCell
+            else { return cell }
+        productCell.backgroundColor = .white
+        return productCell
+    }
+
+}
+// MARK: - + UICollectionViewDelegateFlowLayout
+extension VendingMachineViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfCellsInRow: CGFloat = CGFloat(collectionView.numberOfItems(inSection: indexPath.section)) + 1
+        let side = collectionView.bounds.width/numberOfCellsInRow
+
+        return CGSize(width: side, height: side)
     }
 }
