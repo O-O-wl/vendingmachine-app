d815ff8552315ca5b6b3127b8500cbe88e984097
부엉이 <o.o.wl.soft@gmail.com>
2019-10-16 19:15:17 +0900

    feat: Modify Beverage  to  adopt NSCoding

--- VendingMachineApp/VendingMachineApp/Source/Model/Beverage.swift
+++ VendingMachineApp/VendingMachineApp/Source/Model/Beverage.swift
@@ -21,10 +21,11 @@ extension Product {
     }
 }
 
-class Beverage {
+class Beverage: NSObject, NSCoding {
+    
     // MARK: - Properties
     static let standardTemperature = 20
-
+    
     private let brand: String
     private let capacity: Int
     private let price: Int
@@ -34,9 +35,8 @@ class Beverage {
     private var expirationDate: Date {
         return productDate + storeDuration
     }
-
     private let temperature: Int
-
+    
     // MARK: - Methods
     init(brand: String = "제조사",
          capacity: Int = 0,
@@ -53,15 +53,54 @@ class Beverage {
         self.storeDuration = storeDuration.dayDuration
         self.temperature = temperature
     }
-
-    required convenience init () {
+    
+    override required convenience init () {
         self.init(price: 0,
                   name: "음료")
     }
+    
+    required init?(coder: NSCoder) {
+        self.brand = coder.decodeObject(forKey: Keys.brand.rawValue) as! String
+        self.capacity = coder.decodeInteger(forKey: Keys.capacity.rawValue)
+        self.price = coder.decodeInteger(forKey: Keys.price.rawValue)
+        self.name = coder.decodeObject(forKey: Keys.name.rawValue) as! String
+        self.productDate = coder.decodeObject(forKey: Keys.productDate.rawValue) as! Date
+        self.storeDuration = coder.decodeDouble(forKey: Keys.storeDuration.rawValue)
+        self.temperature = coder.decodeInteger(forKey: Keys.temperature.rawValue)
+    }
+    
+    // MARK: NSCoding
+    func encode(with coder: NSCoder) {
+        coder.encode(brand,
+                     forKey: Keys.brand.rawValue)
+        coder.encode(capacity,
+                     forKey: Keys.capacity.rawValue)
+        coder.encode(price,
+                     forKey: Keys.price.rawValue)
+        coder.encode(name,
+                     forKey: Keys.name.rawValue)
+        coder.encode(productDate,
+                     forKey: Keys.productDate.rawValue)
+        coder.encode(storeDuration,
+                     forKey: Keys.storeDuration.rawValue)
+        coder.encode(temperature,
+                     forKey: Keys.temperature.rawValue)
+    }
+    
+    enum Keys: String {
+        case brand = "Brand"
+        case capacity = "Capacity"
+        case price = "Price"
+        case name = "Name"
+        case productDate = "ProductDate"
+        case storeDuration = "StoreDuration"
+        case temperature = "Temperature"
+    }
+    
 }
 // MARK: - + CustomStringConvertible
-extension Beverage: CustomStringConvertible {
-    var description: String {
+extension Beverage {
+    override var description: String {
         return "\(brand), \(capacity)ml, \(price)원, \(name), \(productDate.text)"
     }
 }
@@ -75,23 +114,17 @@ extension Beverage: Product {
     var productName: String {
         return self.name
     }
-
+    
     var productPrice: Money {
         return Money(value: self.price)
     }
-
+    
     var isHot: Bool {
         return temperature > Beverage.standardTemperature
     }
-
+    
     var isDue: Bool {
         let nowDate = Date()
         return expirationDate < nowDate
     }
-}
-extension Beverage: Hashable {
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(productName)
-    }
-    
 }
