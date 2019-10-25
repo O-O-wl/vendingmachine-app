17856e8a32450ef26df29f7e270e87954f467449
부엉이 <o.o.wl.soft@gmail.com>
2019-10-25 22:24:54 +0900

    feat: Add BaseProductCell that have common logic

--- VendingMachineApp/VendingMachineApp/Source/View/Component/BaseProductCell.swift
+++ VendingMachineApp/VendingMachineApp/Source/View/Component/BaseProductCell.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class BaseProductCell: UICollectionViewCell {
     
+    override func awakeFromNib() {
+           super.awakeFromNib()
+           self.contentView.layer.cornerRadius = self.contentView.frame.width/3
+           self.contentView.backgroundColor = .white
+           self.layer.masksToBounds = false
+           self.layer.shadowColor = UIColor.black.cgColor
+           self.layer.shadowOffset = CGSize(width: 2,
+                                            height: 10)
+           self.layer.shadowRadius = 5
+           self.layer.shadowOpacity = 0.5
+       }
 }
