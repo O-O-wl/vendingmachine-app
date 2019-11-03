13f774b909a8b62d738a1613ae1aa31ddf249dea
부엉이 <o.o.wl.soft@gmail.com>
2019-11-03 19:30:16 +0900

    refactor: Remove code to make it superclass

--- VendingMachineApp/VendingMachineApp/Source/View/Manager/MenuCollectionViewManager.swift
+++ VendingMachineApp/VendingMachineApp/Source/View/Manager/MenuCollectionViewManager.swift
@@ -32,40 +32,14 @@ class MenuCollectionViewManager: NSObject, UICollectionViewDataSource {
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductCell.reuseId,
                                                       for: indexPath)
-        guard
-            let productCell = cell as? ProductCell
-            else { return cell }
+        guard let productCell = cell as? ProductCell else { return cell }
+        
         let model = service.cellForProductAt(index: indexPath.item)
-        productCell.delegate = self
+        
         productCell.indexPath = indexPath
         productCell.configure(product: model)
-        return productCell
-    }
-    
-    func add(_ indexPath: IndexPath) {
-        guard
-            let beverage = service.searchItem(at: indexPath.row)
-            else { return }
-        let strategy = InStockStrategy(stockToAdd: beverage,
-                                       completion: { _ in })
-        service.setStrategy(strategy)
-        do {
-            try service.execute()
-        } catch let error {
-            errorHandler?.handle(error)
-        }
-    }
-    
-    func purchase(_ indexPath: IndexPath) {
-        let strategy = PurchaseStrategy(productToPurchaseIndex: indexPath.row,
-                                        completion: { _, _ in })
-        service.setStrategy(strategy)
-        do {
-            try service.execute()
-        } catch let error {
-            errorHandler?.handle(error)
-        }
         
+        return productCell
     }
 }
 // MARK: - + UICollectionViewDelegateFlowLayout
@@ -74,9 +48,8 @@ extension MenuCollectionViewManager: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let numberOfCellsInRow: CGFloat = CGFloat(collectionView.numberOfItems(inSection: indexPath.section)) + 1
+        let numberOfCellsInRow = CGFloat(collectionView.numberOfItems(inSection: indexPath.section)) + 1
         let side = collectionView.bounds.width/numberOfCellsInRow
-        return CGSize(width: side,
-                      height: side)
+        return CGSize(width: side, height: side)
     }
 }
