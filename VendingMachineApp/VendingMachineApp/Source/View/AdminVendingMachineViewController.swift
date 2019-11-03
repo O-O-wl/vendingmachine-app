b054c00587d523bab64b4574ee2c59de9420a29e
부엉이 <o.o.wl.soft@gmail.com>
2019-11-03 19:10:01 +0900

    Modify CellButtonDelegate, Add CellStyle

---
+++ VendingMachineApp/VendingMachineApp/Source/View/AdminVendingMachineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdminVendingMachineViewController.swift
+//  VendingMachineApp
+//
+//  Created by 이동영 on 2019/11/03.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import UIKit
+
+class AdminVendingMachineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VendingMachineApp/VendingMachineApp/Source/View/Manager/Mode/AdminMenuCollectionViewManager.swift
@@ -0,0 +1,38 @@
+//
+//  AdminCollectionViewManager.swift
+//  VendingMachineApp
+//
+//  Created by 이동영 on 2019/11/03.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AdminMenuCollectionViewManager: MenuCollectionViewManager {
+    
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath)
+        if let cell = cell as? ProductCell {
+            cell.delegate = self
+        }
+        return cell
+    }
+}
+// MARK: + CellButtonDelegate
+extension AdminMenuCollectionViewManager: CellButtonDelegate {
+    
+    func cellButton(_ button: UIButton, didSelectItemAt indexPath: IndexPath) {
+        guard let beverage = service.searchItem(at: indexPath.row) else { return }
+        
+        let strategy = InStockStrategy(stockToAdd: beverage,
+                                       completion: { _ in })
+        service.setStrategy(strategy)
+        do {
+            try service.execute()
+        } catch let error {
+            errorHandler?.handle(error)
+        }
+    }
+}
