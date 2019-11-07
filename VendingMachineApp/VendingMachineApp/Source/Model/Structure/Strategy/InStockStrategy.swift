2fc837edab326b056ed1181d760a499cc76f7747
부엉이 <o.o.wl.soft@gmail.com>
2019-11-07 16:58:27 +0900

    reafactor: Modify code to depend on StateHandleable interface

--- VendingMachineApp/VendingMachineApp/Source/Model/Structure/Strategy/InStockStrategy.swift
+++ VendingMachineApp/VendingMachineApp/Source/Model/Structure/Strategy/InStockStrategy.swift
@@ -8,25 +8,46 @@
 
 import Foundation
 
-struct InStockStrategy: StateHandleable {
-    private let stockToAdd: Beverage
+struct InStockStrategy: StateHandleStrategy {
+    private var stockToAddIndex: Int
     private let completion: (String) -> Void
-
-    init(stockToAdd: Beverage,
-         completion: @escaping (String) -> Void) {
-        self.stockToAdd = stockToAdd
+    private var stockToAdd: Beverage?
+    
+    init(stockToAddIndex: Int,
+         completion: @escaping (String) -> Void = { _ in }) {
+        self.stockToAddIndex = stockToAddIndex
         self.completion = completion
     }
-
+    
+    mutating func setItemIndex(at index: Int) {
+        stockToAddIndex = index
+    }
+    
     func handle(_ before: State) -> Result<State, Error> {
         let state = before
+        guard let stockToAdd = state.inventory.search(at: stockToAddIndex)
+            else { return .failure(InStockError.noSale) }
         state.inventory.addStock(stockToAdd)
-
+        
         return .success(state)
     }
-
+    
     func complete() {
+        guard let stockToAdd = stockToAdd
+            else { return }
         completion(stockToAdd.productName)
     }
-
+    
+    // MARK: - InStockError
+    enum InStockError: LocalizedError {
+        case noSale
+        
+        var errorDescription: String? {
+            switch self {
+            case .noSale:
+                return "판매하는 상품이 아닙니다.❌"
+            }
+        }
+    }
+    
 }
